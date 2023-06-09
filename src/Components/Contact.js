10c8580e6e38baea8ec89dc6b93d8518717dcf36
Tushar <111505346+Tushar6396@users.noreply.github.com>
2023-06-09 14:19:37 +0530

    added services and contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,8 +3,23 @@ import Header from "./Header";
 export default function Contact(){
     return(
         <>
-            
-            <div>Contact page</div>
+            <section className="contact">
+                <div className="heading">
+                    <h2>Contact <span>Me!</span></h2>
+                </div>
+                <div className="contact-content">
+                    <div className="info">
+                        <p className="details"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque itaque aspernatur amet sunt iusto dolorum quas ipsum voluptatibus et perspiciatis accusantium porro, magni eum quisquam fuga, enim tempore sint id.</p>
+                    </div>
+                    <form action="submit" className="form">
+                        <input type="text" placeholder="first-name" />
+                        <input type="text" placeholder="last-name" />
+                        <input type="email" placeholder="email" />
+                        <textarea name="" id="" placeholder="your message"></textarea>
+                        <input type="submit" value="Send Message" className="btn" /> 
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,10 +1,25 @@
-import Header from "./Header";
+// import "./Services.css";
 
 export default function Services(){
     return(
         <>
-            
-            <div>Services page</div>
+        <section className="services">
+            <h2 className="heading">Our <span>Services</span></h2>
+            <div className="main-container">
+                <div className="box">
+                    <h2 className="name">Web Development</h2>
+                    <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi, officiis sit delectus rerum adipisci incidunt sapiente dolores dolor, nesciunt, explicabo architecto placeat consequuntur. Debitis optio deleniti architecto aut dignissimos!</p>
+                </div>
+                <div className="box">
+                    <h2 className="name">Mobile App Development</h2>
+                    <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi, officiis sit delectus rerum adipisci incidunt sapiente dolores dolor, nesciunt, explicabo architecto placeat consequuntur. Debitis optio deleniti architecto aut dignissimos!</p>
+                </div>
+                <div className="box">
+                    <h2 className="name">Website Designing</h2>
+                    <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi, officiis sit delectus rerum adipisci incidunt sapiente dolores dolor, nesciunt, explicabo architecto placeat consequuntur. Debitis optio deleniti architecto aut dignissimos!</p>
+                </div>
+            </div>
+        </section>
         </>
         
     )
